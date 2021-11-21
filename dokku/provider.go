3c96e6dcd5b6030f7207d819a2b5bcfbeb54d9ad
Aaron Stillwell <stillwell.mailbox@gmail.com>
2021-11-21 22:21:24 +0000

    Add dokku 0.26.x support

--- dokku/provider.go
+++ dokku/provider.go
@@ -137,7 +137,7 @@ func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}
 
 	log.Printf("[DEBUG] host version %v", hostVersion)
 
-	testedVersions := ">=0.24.0 <0.26.0"
+	testedVersions := ">=0.24.0 <0.27.0"
 	testedErrMsg := fmt.Sprintf("This provider has not been tested against Dokku version %s. Tested version range: %s", string(found), testedVersions)
 
 	if err == nil {
